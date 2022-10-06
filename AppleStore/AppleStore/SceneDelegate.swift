4e575a880008df13706702b034c07ca8b48b7e2c
Dmitry <suprun1212@gmail.com>
2022-10-06 16:54:32 +0300

    Initial commit

---
+++ AppleStore/AppleStore/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  AppleStore
+//
+//  Created by Дмитрий Супрун on 6.10.22.
+//
+
+import UIKit
+
+/// SceneDelegate
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let rootViewController = ViewController()
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ AppleStore/AppleStore/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AppleStore
+//
+//  Created by Дмитрий Супрун on 6.10.22.
+//
+
+import UIKit
+
+/// UIViewController
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
